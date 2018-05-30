29cef50b847fe34191581b35c83a96c22662cafc
Romanov Michael <romanov.michael.v@gmail.com>
2018-05-30 22:21:28 +0300

    Isolated a training script as a function

---
+++ src/scripts.py
@@ -0,0 +1,118 @@
+import torch
+import torchvision
+import skimage.transform
+import sys
+import argparse
+import time
+import importlib.util
+
+import torchvision.transforms as transform
+
+import trainer
+import utils
+
+def train(model_source_path,
+          dataset_source_path,
+          dataset_path,
+          batch_size=32,
+          num_workers=4,
+          pretraining=False,
+          learning_rate=3.0e-4,
+          dump_period=1,
+          epochs=1000,
+          checkpoint=None,
+          use_cuda=False,
+          validate_on_train=False,
+          max_train_iterations=-1,
+          max_valid_iterations=-1,
+          verbosity=-1,
+          checkpoint_prefix=''):
+    
+    spec = importlib.util.spec_from_file_location("model", model_source_path)
+    model = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(model)
+
+    spec = importlib.util.spec_from_file_location("dataset", dataset_source_path)
+    dataset = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(dataset)
+
+    ## Creating neural network and a socket for it
+
+    net = model.Network()
+    net = torch.nn.parallel.DataParallel(net).eval()
+
+    if use_cuda:
+        net = net.cuda()
+    
+    socket = model.Socket(net)
+
+    train_modules = socket.model.module.train_modules
+    
+    train_optimizer = torch.optim.Adam(
+            train_modules.parameters(), 
+            lr=learning_rate)
+    
+    if pretraining:
+        train_modules = socket.model.module.pretrain_modules
+        checkpoint_prefix = 'pretraining' + checkpoint_prefix
+
+   
+    # Creating the datasets
+ 
+    ds_index = dataset.DataSetIndex(dataset_path)
+    train_dataset = dataset.DataSet(
+        ds_index, mode='train')
+
+    valid_dataset = dataset.DataSet(
+        ds_index, mode='valid')
+
+    ## Creating dataloaders from the datasets
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, 
+                                               batch_size=batch_size,
+                                               shuffle=False,
+                                               num_workers=num_workers,
+                                               drop_last=True,
+                                               pin_memory=False,
+                                               collate_fn=utils.default_collate)
+
+    valid_loader = torch.utils.data.DataLoader(valid_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=False,
+                                               num_workers=num_workers,
+                                               drop_last=False,
+                                               pin_memory=False,
+                                               collate_fn=utils.default_collate)
+
+    
+    my_trainer = trainer.Trainer(socket, 
+                                 train_optimizer,
+                                 train_modules,
+                                 verbosity=verbosity,
+                                 use_cuda=use_cuda,
+                                 max_train_iterations=max_train_iterations,
+                                 max_valid_iterations=max_valid_iterations)
+
+    if checkpoint is not None:
+        my_trainer = trainer.load_from_checkpoint(checkpoint,
+                                                  socket,
+                                                  train_optimizer,
+                                                  train_modules,
+                                                  verbosity=verbosity,
+                                                  use_cuda=use_cuda,
+                                                  max_train_iterations=max_train_iterations,
+                                                  max_valid_iterations=max_valid_iterations)
+        
+        
+    for index in range(epochs):
+        loss = my_trainer.train(train_loader)
+        metrics = []
+        if validate_on_train:
+            for metric in my_trainer.validate(train_loader):
+                metrics.append(metric)
+                    
+        for metric in my_trainer.validate(valid_loader):
+            metrics.append(metric)
+            
+        if index % dump_period == 0:
+            my_trainer.make_checkpoint(checkpoint_prefix, info=metrics)
--- train/train.py
+++ train/train.py
@@ -12,6 +12,7 @@ sys.path.append('../src')
 
 import trainer
 import utils
+import scripts
 
 ## Training parameters
 
@@ -37,104 +38,17 @@ parser.add_argument('-v', '--verbosity',
 parser.add_argument('-cp', '--checkpoint-prefix', help='Prefix to the checkpoint name', default='')
 args = vars(parser.parse_args())
 
-batch_size = args['batch_size']
-num_workers = args['workers']
-pretraining = args['pretraining']
-learning_rate = args['learning_rate']
-dump_period = args['dump_period']
-epochs = args['epochs']
-checkpoint = args['checkpoint']
-use_cuda = args['use_cuda']
-validate_on_train = args['validate_on_train']
-max_train_iterations = args['max_train_iterations']
-max_valid_iterations = args['max_valid_iterations']
-dataset_path = args['dataset_path']
-verbosity = args['verbosity']
-checkpoint_prefix = args['checkpoint_prefix']
-
-spec = importlib.util.spec_from_file_location("model", args['model'])
-model = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(model)
-
-spec = importlib.util.spec_from_file_location("dataset", args['dataset'])
-dataset = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(dataset)
-
-## Making datasets
-if __name__ == '__main__':
-    
-    ds_index = dataset.DataSetIndex(dataset_path)
-    train_dataset = dataset.DataSet(
-        ds_index, mode='train')
-
-    valid_dataset = dataset.DataSet(
-        ds_index, mode='valid')
-
-    ## Creating dataloaders from the datasets
-
-    train_loader = torch.utils.data.DataLoader(train_dataset, 
-                                               batch_size=batch_size,
-                                               shuffle=False,
-                                               num_workers=num_workers,
-                                               drop_last=True,
-                                               pin_memory=False,
-                                               collate_fn=utils.default_collate)
-
-    valid_loader = torch.utils.data.DataLoader(valid_dataset,
-                                               batch_size=batch_size,
-                                               shuffle=False,
-                                               num_workers=num_workers,
-                                               drop_last=False,
-                                               pin_memory=False,
-                                               collate_fn=utils.default_collate)
-
-    ## Creating neural network and a socket for it
-
-    net = model.Network()
-    net = torch.nn.parallel.DataParallel(net).eval()
-
-    if use_cuda:
-        net = net.cuda()
-    
-    socket = model.Socket(net)
-
-    train_modules = socket.model.module.train_modules
-    
-    train_optimizer = torch.optim.Adam(
-            train_modules.parameters(), 
-            lr=learning_rate)
-    
-    if pretraining:
-        train_modules = socket.mode.module.pretrain_modules
-        checkpoint_prefix = 'pretraining' + checkpoint_prefix
-    
-    my_trainer = trainer.Trainer(socket, 
-                                 train_optimizer,
-                                 train_modules,
-                                 verbosity=verbosity,
-                                 use_cuda=use_cuda,
-                                 max_train_iterations=max_train_iterations,
-                                 max_valid_iterations=max_valid_iterations)
-    if checkpoint is not None:
-        my_trainer = trainer.load_from_checkpoint(checkpoint,
-                                                  socket,
-                                                  train_optimizer,
-                                                  train_modules,
-                                                  verbosity=verbosity,
-                                                  use_cuda=use_cuda,
-                                                  max_train_iterations=max_train_iterations,
-                                                  max_valid_iterations=max_valid_iterations)
-        
-        
-    for index in range(epochs):
-        loss = my_trainer.train(train_loader)
-        metrics = []
-        if validate_on_train:
-            for metric in my_trainer.validate(train_loader):
-                metrics.append(metric)
-                    
-        for metric in my_trainer.validate(valid_loader):
-            metrics.append(metric)
-            
-        if index % dump_period == 0:
-                my_trainer.make_checkpoint(checkpoint_prefix, info=metrics)
+scripts.train(args['model'], args['dataset'], args['dataset_path'],
+              batch_size=args['batch_size'],
+              num_workers=args['workers'],
+              pretraining=args['pretraining'],
+              learning_rate=args['learning_rate'],
+              dump_period=args['dump_period'],
+              epochs=args['epochs'],
+              checkpoint=args['checkpoint'],
+              use_cuda=args['use_cuda'],
+              validate_on_train=args['validate_on_train'],
+              max_train_iterations=args['max_train_iterations'],
+              max_valid_iterations = args['max_valid_iterations'],
+              verbosity = args['verbosity'],
+              checkpoint_prefix = args['checkpoint_prefix'])
