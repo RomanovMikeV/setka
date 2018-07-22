b7334ece4787f9055b4d0b74dcacb625716232a4
RomanovMikeV <romanov.michael.v@gmail.com>
2018-07-22 15:26:45 +0300

    Renaming

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="scorch",
+    name="pytorch-scorch",
     version="0.0.1",
     author="Mikhail Romanov",
     author_email="romanov.michael.v@gmail.com",
