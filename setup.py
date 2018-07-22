d2425a163b36e1184b01c93ded51e83e2418f2fc
RomanovMikeV <romanov.michael.v@gmail.com>
2018-07-22 14:42:39 +0300

    Prepared for PIP installation

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scorch",
+    version="0.0.1",
+    author="Mikhail Romanov",
+    author_email="romanov.michael.v@gmail.com",
+    description="A set of scripts to make the Neural Network training with pytorch faster",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RomanovMikeV/scorch",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
