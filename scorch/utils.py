bd022986b0f3b73008d7e19bf5405ae35141683f
mike <romanov.michael.v@gmail.com>
2019-02-11 13:23:32 +0300

    Added util for denormalization of the image

--- scorch/utils.py
+++ scorch/utils.py
@@ -11,3 +11,18 @@ def numpy2torch(image):
     res = image.clone()
     res = res.swapaxes(2, 1).swapaxes(1, 0)
     return res
+
+
+def denorm(image, int_image=False,
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]):
+    res = image.clone()
+    std = numpy.array(std)
+    mean = numpy.array(mean)
+
+    if int_image:
+        std *= 255.0
+        mean *= 255.0
+
+    res = res * std + mean
+    return res
