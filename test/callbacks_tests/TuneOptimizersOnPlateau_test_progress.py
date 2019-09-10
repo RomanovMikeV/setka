f58aaefad1c6119566b802852f15d295642c43e0
RomanovMikeV <romanov.michael.v@gmail.com>
2019-09-11 00:59:27 +0300

    Now there should be a progress in TuneOptimizers test

--- test/callbacks_tests/TuneOptimizersOnPlateau_test_progress.py
+++ test/callbacks_tests/TuneOptimizersOnPlateau_test_progress.py
@@ -25,7 +25,7 @@ ds = test_dataset.CIFAR10()
 model = tiny_model.TensorNet()
 
 trainer = setka.base.Trainer(callbacks=[
-                                 setka.callbacks.DataSetHandler(ds, batch_size=32, limits=2),
+                                 setka.callbacks.DataSetHandler(ds, batch_size=32, limits=10),
                                  setka.callbacks.ModelHandler(model),
                                  setka.callbacks.LossHandler(loss),
                                  setka.callbacks.OneStepOptimizers(
@@ -33,7 +33,7 @@ trainer = setka.base.Trainer(callbacks=[
                                         setka.base.OptimizerSwitch(
                                             model,
                                             torch.optim.SGD,
-                                            lr=0.1,
+                                            lr=0.01,
                                             momentum=0.9,
                                             weight_decay=5e-4)
                                     ]
