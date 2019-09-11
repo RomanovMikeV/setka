17c1784c29e481fb2c402446e7467538028dc5fe
RomanovMikeV <romanov.michael.v@gmail.com>
2019-09-12 00:42:25 +0300

    base init update

--- setka/base/__init__.py
+++ setka/base/__init__.py
@@ -2,3 +2,5 @@ from .DataSet import DataSet
 from .DataSetWrapper import DataSetWrapper
 from .OptimizerSwitch import OptimizerSwitch
 from .Trainer import Trainer
+
+from .environment_setup import environment_setup
