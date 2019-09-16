d1ac9b62ac628ef45d2416cbb91692ba67116b47
RomanovMikeV <romanov.michael.v@gmail.com>
2019-09-17 01:37:00 +0300

    Added three datasets: CIFAR10, CIFAR100, TinyImageNet

---
+++ setka/datasets/CIFAR10.py
@@ -0,0 +1,42 @@
+import torchvision.datasets
+
+class CIFAR10(setka.base.DataSet):
+    def __init__(self,
+                 root='~/datasets'):
+
+        train_transforms = torchvision.transforms.Compose([
+            torchvision.transforms.RandomCrop(32, padding=4),
+            torchvision.transforms.RandomHorizontalFlip(),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+
+        test_transforms = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+
+        self.train_data = torchvision.datasets.CIFAR10(
+            '~/datasets', train=True, download=True,
+            transform=train_transforms)
+        self.test_data = torchvision.datasets.CIFAR10(
+            '~/datasets', train=False, download=True,
+            transform=test_transforms)
+
+        self.n_valid = int(0.05 * len(self.train_data))
+
+    def getlen(self, subset, index):
+        if subset == 'train':
+            return len(self.train_data) - self.n_valid
+        elif subset == 'valid':
+            return self.n_valid
+        elif subset == 'test':
+            return len(self.test_data)
+
+    def getitem(self, subset, index):
+        if subset == 'train':
+            return self.train_data[self.n_valid + index]
+        elif subset == 'valid':
+            return self.train_data[index]
+        elif subset == 'test':
+            return self.test_data[index]
