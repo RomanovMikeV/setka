d8a8b9aa8bd63eacabc6445b6b210af338fbe444
Michael Romanov <RomanovMikeV@users.noreply.github.com>
2019-10-07 15:22:06 +0300

    Update UseCuda.py

--- setka/callbacks/UseCuda.py
+++ setka/callbacks/UseCuda.py
@@ -1,14 +1,29 @@
 from .Callback import Callback
 import torch
 
-class UseCUDA(Callback):
+class UseCuda(setka.callbacks.Callback):
+    @staticmethod
+    def move2cuda(coll):
+        if isinstance(coll, dict):
+            for key in coll:
+                coll[key] = coll[key].cuda()
+            return coll
+                
+        elif isinstance(coll, (list, tuple)):
+            new_coll = []
+            for val in coll:
+                new_coll.append(val.cuda())
+            return new_coll
+                
+        else:
+            coll = coll.cuda()
+            return coll
+        
     def __init__(self):
         pass
-
-    def on_init(self):
+    
+    def on_epoch_begin(self):
         self.trainer._model.cuda()
-
+    
     def on_batch_begin(self):
-        if torch.cuda.is_available():
-            for index in range(len(self.trainer._input)):
-                self.trainer._input[index] = self.trainer._input[index].cuda()
+        self.move2cuda(self.trainer._input)
