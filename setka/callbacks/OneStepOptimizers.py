e1c33ee8d957e8af1a78e0f101b37f75c41be920
Michael Romanov <RomanovMikeV@users.noreply.github.com>
2019-10-13 16:25:35 +0300

    Update OneStepOptimizers.py

--- setka/callbacks/OneStepOptimizers.py
+++ setka/callbacks/OneStepOptimizers.py
@@ -8,12 +8,13 @@ class OneStepOptimizers(Callback):
         self.trainer._optimizers = self.optimizers
 
     def on_batch_begin(self):
-        for optimizer in self.trainer._optimizers:
-            optimizer.optimizer.zero_grad()
+        if self.trainer._mode == 'train':
+            for optimizer in self.trainer._optimizers:
+                optimizer.optimizer.zero_grad()
 
-        # Switch the necessary layers to training mode
-        for optimizer in self.trainer._optimizers:
-            optimizer.module.train()
+            # Switch the necessary layers to training mode
+            for optimizer in self.trainer._optimizers:
+                optimizer.module.train()
 
     def on_batch_end(self):
         for optimizer in self.trainer._optimizers:
