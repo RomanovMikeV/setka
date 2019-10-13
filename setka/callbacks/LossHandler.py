2ec01900d363982069022c0faf001c4e3e08102f
Michael Romanov <RomanovMikeV@users.noreply.github.com>
2019-10-13 16:48:51 +0300

    Update LossHandler.py

--- setka/callbacks/LossHandler.py
+++ setka/callbacks/LossHandler.py
@@ -7,13 +7,13 @@ class LossHandler(Callback):
         self.set_priority({'on_batch_run': 9, 'on_batch_end': -9})
 
     def on_batch_run(self):
-        if self.trainer.status["mode"] in ["train", "valid"]:
+        if self.trainer._mode in ["train", "valid"]:
             self.trainer._loss = self.criterion(self.trainer._output, self.trainer._input)
-            if self.trainer.status["mode"] == "train":
+            if self.trainer._mode == "train":
                 self.trainer._loss.backward()
 
             self.trainer.status['loss'] = self.trainer._loss.detach().cpu().item()
 
     def on_batch_end(self):
-        if self.trainer.status["mode"] in ["train", "valid"]:
+        if self.trainer._mode in ["train", "valid"]:
             del self.trainer._loss
