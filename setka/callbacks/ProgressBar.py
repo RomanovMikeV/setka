b631d520be08886c324de010530e3dca061f0c1f
Michael Romanov <RomanovMikeV@users.noreply.github.com>
2019-10-14 13:43:18 +0300

    Update ProgressBar.py

--- setka/callbacks/ProgressBar.py
+++ setka/callbacks/ProgressBar.py
@@ -7,20 +7,6 @@ class ProgressBar(Callback):
     def __init__(self, mode='bash'):
         self.set_priority(-100)
         self.mode = mode
-        if mode == 'notebook':
-            self.pbar = tqdm.tqdm_notebook(
-                range(100),
-                leave=False,
-                bar_format='{n}/|/{percentage:3.0f}% [{elapsed}>{remaining}] {rate_fmt} {postfix}')
-        elif mode == 'bash':
-            self.pbar = tqdm.tqdm(
-                range(100),
-                ascii=True,
-                leave=False,
-                bar_format='{percentage:3.0f}% [{elapsed}>{remaining}] {rate_fmt} {postfix}'
-            )
-
-            print(self.pbar.bar_format)
 
 
     @staticmethod
@@ -35,14 +21,29 @@ class ProgressBar(Callback):
         return res
 
     def on_epoch_begin(self):
+        if mode == 'notebook':
+            self.pbar = tqdm.tqdm_notebook(
+                range(100),
+                leave=False,
+                bar_format='{n}/|/{percentage:3.0f}% [{elapsed}>{remaining}] {rate_fmt} {postfix}')
+        elif mode == 'bash':
+            self.pbar = tqdm.tqdm(
+                range(100),
+                ascii=True,
+                leave=False,
+                bar_format='{percentage:3.0f}% [{elapsed}>{remaining}] {rate_fmt} {postfix}')
+        
         self.pbar.clear()
         self.pbar.n = 0
         self.pbar.start_t = time.time()
 
+        
     def on_epoch_end(self):
         self.status_string = '  '.join([str(k) + ': ' + self.format(v) for k, v in self.trainer.status.items()])
-        if hasattr(self, 'status_string'):
-            self.pbar.write(self.status_string)
+        self.pbar.close()
+        print(self.status_string)
+        #if hasattr(self, 'status_string'):
+        #    self.pbar.write(self.status_string)
 
     def on_batch_end(self):
 
@@ -52,6 +53,3 @@ class ProgressBar(Callback):
         self.status_string = '  '.join([str(k) + ': ' + self.format(v) for k, v in self.trainer.status.items()])
 
         self.pbar.set_postfix_str(self.status_string)
-
-    def on_train_end(self):
-        self.pbar.close()
