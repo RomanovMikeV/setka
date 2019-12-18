ea6bcaa6e4bfc826dd4597501bdf8e08a948c1a2
nikolay.patakin <nikolay.patakin@graphics.cs.msu.ru>
2019-12-18 23:12:48 +0300

    Overall:     - Improved PEP code style guide complience     - Slight refactoring     - Typos in docstrings corrected
    Features:
        - LossHandler: "retain_graph" parameter support
        - ModelHandler: DataParallel handler is optional now. You can specify device_ids parameter to specify allowed gpu ids for training
        - UseCuda: "device" parameter support. More flexible data transfer method (np.ndarray, scalar values, nested lists and dictionaries support)
    
    Bug fixes:
        - DatasetHandler never used shuffle before
        - Logger had no tensors detach before log function call (possible memory leak)

--- setka/pipes/LossHandler.py
+++ setka/pipes/LossHandler.py
@@ -1,7 +1,8 @@
 from .Pipe import Pipe
 
+
 class LossHandler(Pipe):
-    '''
+    """
     Handles loss functions.
 
     Stores:
@@ -9,26 +10,29 @@ class LossHandler(Pipe):
 
     Args:
         criterion (callable): loss function
-    '''
-    def __init__(self, criterion):
+        retain_graph (bool): Retain graph in criterion backward call
+    """
+    def __init__(self, criterion, retain_graph=None):
+        super(LossHandler, self).__init__()
         self.criterion = criterion
+        self.retain_graph = retain_graph
 
         self.set_priority({'on_batch': 9, 'after_batch': -9})
 
     def on_batch(self):
-        '''
+        """
         Computes loss in case self.trainer is in mode 'train' or 'valid'.
-        '''
+        """
         if self.trainer._mode in ["train", "valid"]:
             self.trainer._loss = self.criterion(self.trainer._output, self.trainer._input)
             if self.trainer._mode == "train":
-                self.trainer._loss.backward()
+                self.trainer._loss.backward(retain_graph=self.retain_graph)
 
             self.trainer.status['loss'] = self.trainer._loss.detach().cpu().item()
 
     def after_batch(self):
-        '''
+        """
         Releases loss value in case it is present.
-        '''
+        """
         if self.trainer._mode in ["train", "valid"]:
             del self.trainer._loss
--- setka/pipes/UseCuda.py
+++ setka/pipes/UseCuda.py
@@ -1,39 +1,44 @@
-from .Pipe import Pipe
 import torch
+import numpy as np
+
+from .Pipe import Pipe
+
 
 class UseCuda(Pipe):
-    '''
+    """
     This pipe moves the tensors and the model to cuda when it is needed.
-    '''
+    """
+    def __init__(self, device='cuda:0'):
+        super(UseCuda, self).__init__()
+        self.device = device
 
-    @staticmethod
-    def move2cuda(coll):
+    def move2cuda(self, coll):
         if isinstance(coll, dict):
             for key in coll:
-                coll[key] = coll[key].cuda()
-            return coll
-                
+                coll[key] = self.move2cuda(coll[key])
         elif isinstance(coll, (list, tuple)):
             new_coll = []
             for val in coll:
-                new_coll.append(val.cuda())
-            return new_coll
-                
+                new_coll.append(self.move2cuda(val))
+            coll = new_coll
+        elif isinstance(coll, torch.Tensor):
+            coll = coll.to(device=self.device)
         else:
-            coll = coll.cuda()
-            return coll
-        
-    def __init__(self):
-        pass
-    
+            try:
+                coll = torch.as_tensor(coll).to(device=self.device)
+            except RuntimeError:
+                raise RuntimeError('Unknown data type used as input: ' + str(type(coll)) + '. Can`t move to CUDA')
+
+        return coll
+
     def before_epoch(self):
-        '''
+        """
         Moves model to GPU.
-        '''
-        self.trainer._model.cuda()
+        """
+        self.trainer._model.to(device=self.device)
     
     def before_batch(self):
-        '''
+        """
         Moves batch to GPU.
-        '''
+        """
         self.move2cuda(self.trainer._input)
